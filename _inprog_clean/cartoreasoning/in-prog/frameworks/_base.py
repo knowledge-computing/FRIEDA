c93f4db1a60098d86e4cba5878c5e708accf0332
YOO-uN-ee <jiyoonp0228@gmail.com>
2026-01-28 12:40:22 -0600

    output results

--- _inprog_clean/cartoreasoning/in-prog/frameworks/_base.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from abc import ABC, abstractmethod
-
-class DecoderBase(ABC):
-    def __init__() -> None:
-        return 0
-
-    def respond_q() -> List[str]:
-        return 0
